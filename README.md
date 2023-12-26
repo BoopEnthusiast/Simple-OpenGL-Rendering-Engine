75df9fcf03c8f1800ffd405fb518663a5ef82d01
Boop <onpotis@gmail.com>
2023-12-27 02:24:06 +1300

    SHADERS WOOO

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Simple OpenGL Rendering Engine
- My CS50x final project
+# Simple OpenGL renderer
+#### Video Demo:  <URL HERE>
+#### Description:
+A large portion of the code is from various sources other than myself:
+Initial window from glfw example code: https://www.glfw.org/documentation.html
+To help learn the beginnings I followed The Cherno's OpenGL series on youtube: https://youtube.com/playlist?list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&si=u6WYzXmiBVdamIYa
+Also used a bit of the glew example code: https://glew.sourceforge.net/basic.html
--- Simple-OpenGL-Rendering-Engine/src/Application.cpp
+++ Simple-OpenGL-Rendering-Engine/src/Application.cpp
@@ -1,4 +1,90 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+struct ShaderProgramSource
+{
+    std::string VertexSource;
+    std::string FragmentSource;
+};
+
+static ShaderProgramSource ParseShader(const std::string& filepath)
+{
+    std::ifstream stream(filepath);
+
+    enum class ShaderType
+    {
+        NONE = -1, 
+        VERTEX = 0, 
+        FRAGMENT = 1
+    };
+
+    std::string line;
+    std::stringstream ss[2];
+    ShaderType type = ShaderType::NONE;
+    while (getline(stream, line))
+    {
+        std::cout << line << (line.find("#shader") != std::string::npos) << std::endl;
+        if (line.find("#shader") != std::string::npos)
+        {
+            if (line.find("vertex") != std::string::npos)
+                type = ShaderType::VERTEX;
+            else if (line.find("fragment") != std::string::npos)
+                type = ShaderType::FRAGMENT;
+        }
+        else
+        {
+            ss[(int)type] << line << std::endl;
+        }
+    }
+
+    return { ss[0].str(), ss[1].str() };
+}
+
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to compile: " << (type == GL_VERTEX_SHADER ? "Vertex" : "Fragment") << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = CompileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
 
 int main(void)
 {
@@ -8,10 +94,13 @@ int main(void)
     if (!glfwInit())
         return -1;
 
+   
+
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "My OpenGL window", NULL, NULL);
     if (!window)
     {
+        std::cout << "Yeah that window didn't work there hun... sorry" << std::endl;
         glfwTerminate();
         return -1;
     }
@@ -19,17 +108,43 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    // Check that glew initialized well
+    if (glewInit() != GLEW_OK)
+        std::cout << "Error from glew init!!" << std::endl;
+    
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << 
+        "\nOpenGL shading language version: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << std::endl;
+
+    // Define the positions of the vertecies
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.5f, -0.5f,
+         0.0f,  0.5f
+    };
+
+    // Create the vertex buffer
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    std::cout << "Parsing shader" << std::endl;
+    ShaderProgramSource shaderSource = ParseShader("res/shaders/Basic.shader");
+    std::cout << "Vertex source:\n" << shaderSource.VertexSource << "\nFragment source:\n" << shaderSource.FragmentSource << std::endl;
+    
+    unsigned int shader = CreateShader(shaderSource.VertexSource, shaderSource.FragmentSource);
+    glUseProgram(shader);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f( 0.5f, -0.5f);
-        glVertex2f( 0.0f,  0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -37,6 +152,8 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
+
+    glDeleteProgram(shader);
 
     glfwTerminate();
     return 0;
